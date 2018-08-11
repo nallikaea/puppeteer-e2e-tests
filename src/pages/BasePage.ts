c0e1e272d0c1892d0f265c849267d9cd5ff6364a
James Davis <jrdavismaine@gmail.com>
2018-08-11 12:00:35 -0400

    Fixed getPageHeader bug

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,3 +1,4 @@
+const baseSelectors = require('../selectors/basePage.json');
 export default class BasePage {
     page: any;
     constructor(page) {
@@ -6,13 +7,10 @@ export default class BasePage {
 
     async getPageHeader() : Promise<String> {
         // The header is either an h2 or h3 element.
-        const h2Selector = 'document.querySelector("div.example h2");';
-        const h3Selector = 'document.querySelector("div.example h3");';
         try {
-            return await this.page.evaluate(h2Selector).textContent;
+            return await this.page.$eval(baseSelectors.level2Header, e => e.innerText);
         } catch (err) {
-            // assume h2 is not the page header
-            return await this.page.evaluate(h3Selector).textContent;
+            return await this.page.$eval(baseSelectors.level3Header, e => e.innerText);
         }
     }
 }
