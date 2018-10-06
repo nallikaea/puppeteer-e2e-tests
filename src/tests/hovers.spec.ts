dca99a6d63656417480c1c6270784e3a19beee34
James Davis <jrdavismaine@gmail.com>
2018-10-06 09:35:50 -0400

    Implemented it.each for test

--- src/tests/hovers.spec.ts
+++ src/tests/hovers.spec.ts
@@ -1,50 +1,34 @@
 import { expect } from 'chai';
 import NavigationMenu from '../pages/NavigationMenu';
 import launchPuppeteer from '../utils/launchPuppeteer';
+require('it-each')();
+require('it-each')({ testPerIteration: true });
 
-// TODO figure out way to loop thgrough tests async, this suite violates DNRY rule!
-// it.each cannot be used because of the lack of typings file, what else is out there for TypeScript?
 describe('Hovers Test', () => {
     let browser;
     let page;
-    let hoversPage;
+    let navMenu;
 
     before(async () => {
         browser = await launchPuppeteer();
         page = await browser.newPage();
         await page.setViewport({ width: 1280, height: 1024 });
-        const navMenu = new NavigationMenu(page);
-        hoversPage = await navMenu.loadHoversPage();
+        navMenu = new NavigationMenu(page);
     });
 
     after(async () => {
         await browser.close();
     });
 
-    it('Hover over images test - user 1', async () => {
-        const userNumber = 0;
+    // @ts-ignore: Property 'each' does not exist on type 'ITestDefinition'.
+    // it-each does not have a typing file as of July 2018.
+    it.each([0, 1, 2], 'Hover over images test, user %s', ['element'], async (userNumber, next) => {
+        const hoversPage = await navMenu.loadHoversPage();
         await hoversPage.hoverOverImage(userNumber);
         const username = await hoversPage.getUserName(userNumber);
         expect(username).to.equal(`user${userNumber + 1}`);
         await hoversPage.clickViewProfile(userNumber);
         await hoversPage.returnToHoversPage();
-    });
-
-    it('Hover over images test - user 2', async () => {
-        const userNumber = 1;
-        await hoversPage.hoverOverImage(userNumber);
-        const username = await hoversPage.getUserName(userNumber);
-        expect(username).to.equal(`user${userNumber + 1}`);
-        await hoversPage.clickViewProfile(userNumber);
-        await hoversPage.returnToHoversPage();
-    });
-
-    it('Hover over images test - user 3', async () => {
-        const userNumber = 2;
-        await hoversPage.hoverOverImage(userNumber);
-        const username = await hoversPage.getUserName(userNumber);
-        expect(username).to.equal(`user${userNumber + 1}`);
-        await hoversPage.clickViewProfile(userNumber);
-        await hoversPage.returnToHoversPage();
+        next();
     });
 });
