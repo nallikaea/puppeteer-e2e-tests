feb4cd5c3cccf71246b6a18e256a4e51b752c3a3
James Davis <jrdavismaine@gmail.com>
2018-10-07 20:01:24 -0400

    Created JSAlert page scaffolding

---
+++ src/pages/JavaScriptAlerts.ts
@@ -0,0 +1,8 @@
+import BasePage from './BasePage';
+import { ElementHandle } from 'puppeteer';
+
+export default class JavaScriptAlerts extends BasePage {
+    constructor(page) {
+        super(page);
+    }
+}
