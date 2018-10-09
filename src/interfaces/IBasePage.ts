7d085e74e2785878e726a4a69a9e8f5427437789
James Davis <jrdavismaine@gmail.com>
2018-10-09 07:18:46 -0400

    Added BasePage interface

--- src/interfaces/IBasePage.ts
+++ src/interfaces/IBasePage.ts
@@ -2,4 +2,8 @@ import { ElementHandle } from 'puppeteer';
 
 export default interface IBasePage {
     getPageHeader() : Promise<String>;
+    getPageSubHeader() : Promise<String>;
+    getFooterText() : Promise<String>;
+    getPageMessage() : Promise<String>;
+    closePageMessage() : Promise<void>;
 }
--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,5 +1,7 @@
+import IBasePage from '../interfaces/IBasePage';
+
 const baseSelectors = require('../selectors/basePage.json');
-export default class BasePage {
+export default class BasePage implements IBasePage {
     public page: any;
     protected baseSelectors: any;
 
@@ -8,7 +10,7 @@ export default class BasePage {
         this.baseSelectors = baseSelectors;
     }
 
-    protected async getPageHeader() : Promise<String> {
+    public async getPageHeader() : Promise<String> {
         // The header is either an h2 or h3 element.
         try {
             return await this.page.$eval(this.baseSelectors.level2Header, e => e.textContent.trim());
@@ -17,20 +19,20 @@ export default class BasePage {
         }
     }
 
-    protected getPageSubHeader() : Promise<String> {
+    public getPageSubHeader() : Promise<String> {
         return this.page.$eval(this.baseSelectors.pageSubHeaderLocator, e => e.textContent.trim());
     }
 
-    protected getFooterText() : Promise<String> {
+    public getFooterText() : Promise<String> {
         return this.page.$eval(this.baseSelectors.footerTextLocator, e => e.textContent.trim());
     }
 
-    protected getPageMessage() : Promise<String> {
+    public getPageMessage() : Promise<String> {
         const selector = this.baseSelectors.pageMessageLocator;
         return this.page.$eval(selector, e => e.innerText.replace(/[^a-zA-Z0-9! ]+/g, '').trim());
     }
 
-    protected async closePageMessage() : Promise<void> {
+    public async closePageMessage() : Promise<void> {
         await this.page.click(this.baseSelectors.closeButton);
         await this.page.waitForSelector(this.baseSelectors.closeButton, { hidden: true });
     }
