02e75c6e3e91e24c1c93dd0690d9632ad74ec092
James Davis <jrdavismaine@gmail.com>
2018-10-09 07:31:45 -0400

    Added getter methods to IHoversPage

--- src/interfaces/IHoversPage.ts
+++ src/interfaces/IHoversPage.ts
@@ -1,6 +1,9 @@
 import { ElementHandle } from 'puppeteer';
 
 export default interface IHoversPage {
+    readonly captionLinks : Promise<Array<ElementHandle>>;
+    readonly captionUsernames : Promise<Array<ElementHandle>>;
+    readonly figures : Promise<Array<ElementHandle>>;
     hoverOverImage(position) : Promise<void>;
     returnToHoversPage() : Promise<void>;
     clickViewProfile(position) : Promise<void>;
