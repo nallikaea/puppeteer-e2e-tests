ded93f3bbee61bc9d0003bbe677ee7553718f1ed
James Davis <jrdavismaine@gmail.com>
2018-10-12 23:17:38 -0400

    Implemented <4 ASEERT per TC

--- src/tests/checkboxes.spec.ts
+++ src/tests/checkboxes.spec.ts
@@ -8,6 +8,8 @@ describe('Checkboxes test:', () => {
     let page;
     let navMenu;
     let checkboxes;
+    let checkbox1;
+    let checkbox2;
 
     before(async () => {
         browser = await launchPuppeteer();
@@ -17,39 +19,35 @@ describe('Checkboxes test:', () => {
     });
 
     beforeEach(async () => {
-        await navMenu.loadNavigationMenu();
         checkboxes = await navMenu.loadCheckboxesPage();
+        checkbox1 = await checkboxes.checkboxOne();
+        checkbox2 = await checkboxes.checkboxTwo();
     });
 
     after(async () => {
         await browser.close();
     });
 
-    it('Toggle Checkboxes test', async () => {
+    it('Default state test', async () => {
         expect(await checkboxes.getPageHeader()).to.equal('Checkboxes');
-        const checkbox1 = await checkboxes.checkboxOne();
-        const checkbox2 = await checkboxes.checkboxTwo();
         expect(await page.evaluate(e => e.checked, checkbox1)).to.be.false;
         expect(await page.evaluate(e => e.checked, checkbox2)).to.be.true;
+    });
+
+    it('Reverse default state', async () => {
         await checkbox1.click();
         await checkbox2.click();
         expect(await page.evaluate(e => e.checked, checkbox1)).to.be.true;
         expect(await page.evaluate(e => e.checked, checkbox2)).to.be.false;
     });
 
-    it('Checkbox 1 test ', async () => {
-        // Checkbox 1 is not selected by default
-        const checkbox1 = await checkboxes.checkboxOne();
-        const checkbox2 = await checkboxes.checkboxTwo();
+    it('Both boxes checked', async () => {
         await checkbox1.click();
         expect(await page.evaluate(e => e.checked, checkbox1)).to.be.true;
         expect(await page.evaluate(e => e.checked, checkbox2)).to.be.true;
     });
 
     it('Checkbox 2 test ', async () => {
-        // Checkbox 2 is selected by default
-        const checkbox1 = await checkboxes.checkboxOne();
-        const checkbox2 = await checkboxes.checkboxTwo();
         await checkbox2.click();
         expect(await page.evaluate(e => e.checked, checkbox1)).to.be.false;
         expect(await page.evaluate(e => e.checked, checkbox2)).to.be.false;
